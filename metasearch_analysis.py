00a088f4772ec9403925b2e8a0155aa67b32f89f
Caleb Sponheim <caleb.sponheim@gmail.com>
2017-03-16 08:51:10 -0400

    changed metasearch_analysis.py text

--- metasearch_analysis.py
+++ metasearch_analysis.py
@@ -1,13 +1,1 @@
-# coding: utf-8
-# Download our data from OpenNeuroLab
-# get_ipython().system('git clone https://github.com/OpenNeuroLab/metasearch.git')
-from pathlib import Path
-metasearch_dir = Path('data_not_in_repo/metasearch/')
-data_exists = metasearch_dir.exists()
-in_repro_course = Path.cwd().name == 'repro_course'
-if data_exists and in_repro_course:
-    print('data has been downloaded')
-else:
-    print('data has not been found. at all.')
-
-print('end')
+# coding: utf-8import pandas as pddf_pheno = pd.read_csv('https://raw.githubusercontent.com/OpenNeuroLab/metasearch/master/docs/data/phenotype_mri.csv')
